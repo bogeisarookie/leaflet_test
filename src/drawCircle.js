f53fd5648afd772a08fae2604711ec9bef8e17be
boge <1771489181@@qq.com>
2018-01-04 22:26:47 +0800

    将实验室电力可视化初步代码拷贝到宿舍电脑
    Signed-off-by: boge <1771489181@@qq.com>

---
+++ src/drawCircle.js
@@ -0,0 +1,130 @@
+// 绘制泡泡图用于叠加到地图
+
+var overlay = new L.echartsLayer3(map, echarts);
+var chartsContainer = overlay.getEchartsContainer();
+var myChart = overlay.initECharts(chartsContainer);
+
+
+var jsonData = undefined;
+try {
+    jsonData = JSON.parse(document.getElementById("jsonInputField").value);
+} catch (e) { }
+var root = d3.hierarchy(jsonData)
+    .sum(function (d) { return d.size; })//计算每个节点的值，该值包括他自己和他后代的值。
+    .sort(function (a, b) { return b.size - a.size; });//从大到小排序
+var leafNodes = root.descendants().filter(function (candidate) {
+    return !candidate.children;
+});
+
+var convertData = function (data) {
+    var res = [];
+    for (var i = 0; i < data.length; i++) {
+        var coor_value = [data[i].data.lx, data[i].data.ly];
+        res.push({
+            name: data[i].data.name,
+            value: coor_value.concat(data[i].data.size)
+        });
+    }
+    return res;
+};
+
+option = {
+    // backgroundColor: '#404a59',
+    title: {
+        text: '2017合肥市电力数据',
+        subtext: 'Data from PM25.in,Develop By WanderGIS',
+        left: 'center',
+        textStyle: {
+            color: '#fff'
+        }
+    },
+    tooltip: {
+        trigger: 'item'
+    },
+    legend: {
+        orient: 'vertical',
+        y: 'bottom',
+        x: 'left',
+        data: ['pm2.5'],
+        textStyle: {
+            color: '#fff'
+        }
+    },
+    geo: {
+        map: '',
+        label: {
+            emphasis: {
+                show: false
+            }
+        },
+        roam: true,
+        itemStyle: {
+            normal: {
+                areaColor: '#323c48',
+                borderColor: '#111'
+            },
+            emphasis: {
+                areaColor: '#2a333d'
+            }
+        }
+    },
+    series: [
+        {
+            name: 'pm2.5',
+            type: 'scatter',
+            coordinateSystem: 'geo',
+            data: convertData(leafNodes),
+            symbolSize: function (val) {
+                return val[2] / 10;
+            },
+            label: {
+                normal: {
+                    formatter: '{b}',
+                    position: 'right',
+                    show: false
+                },
+                emphasis: {
+                    show: true
+                }
+            },
+            itemStyle: {
+                normal: {
+                    color: '#ddb926'
+                }
+            }
+        },
+        {
+            name: 'Top 5',
+            type: 'effectScatter',
+            coordinateSystem: 'geo',
+            data: convertData(leafNodes.sort(function (a, b) {
+                return b.data.size - a.data.size;
+            }).slice(0, 6)),
+            symbolSize: function (val) {
+                return val[2] / 10;
+            },
+            showEffectOn: 'render',
+            rippleEffect: {
+                brushType: 'stroke'
+            },
+            hoverAnimation: true,
+            label: {
+                normal: {
+                    formatter: '{b}',
+                    position: 'right',
+                    show: true
+                }
+            },
+            itemStyle: {
+                normal: {
+                    color: '#f4e925',
+                    shadowBlur: 10,
+                    shadowColor: '#333'
+                }
+            },
+            zlevel: 1
+        }
+    ]
+};
+// 使用刚指定的配置项和数据显示图表。
+overlay.setOption(option);
---
+++ src/loadMap.js
@@ -0,0 +1,23 @@
+//加载地图
+var map = L.map('map');
+var baseLayers = {
+    "高德地图": L.tileLayer('http://webrd0{s}.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=8&x={x}&y={y}&z={z}', {
+        subdomains: "1234"
+    }),
+    '高德影像': L.layerGroup([L.tileLayer('http://webst0{s}.is.autonavi.com/appmaptile?style=6&x={x}&y={y}&z={z}', {
+        subdomains: "1234"
+    }), L.tileLayer('http://t{s}.tianditu.cn/DataServer?T=cta_w&X={x}&Y={y}&L={z}', {
+        subdomains: "1234"
+    })]),
+    'GeoQ灰色底图': L.tileLayer('http://map.geoq.cn/ArcGIS/rest/services/ChinaOnlineStreetPurplishBlue/MapServer/tile/{z}/{y}/{x}').addTo(map)
+};
+L.tileLayer('https://a.tiles.mapbox.com/v3/foursquare.map-0y1jh28j/{z}/{x}/{y}.png', {
+    attribution: 'Map &copy; Pacific Rim Coordination Center (PRCC).  Certain data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+    key: 'BC9A493B41014CAABB98F0471D759707',
+    styleId: 22677
+});
+var layercontrol = L.control.layers(baseLayers,{}, {
+    position: "topleft"
+}).addTo(map);
+//合肥工业大学
+map.setView(L.latLng(31.8423836382,117.2966909409), 13);
